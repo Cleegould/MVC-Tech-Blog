c976b222c0ff80e4204b5a459fb0730d99c62bd7
chrisg <cleegould@gmail.com>
2023-05-01 17:47:25 -0400

    small adding git ignore

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,16 +1,6 @@
 const { User } = require('../models');
 
-const userData = [
-    {
-        username: "test",
-        email: "test@yahoo.com",
-        password: "12345"
-    },    
-{
-    username: "ceegeezy",
-    email: "ncohearnt@yahoo.com",
-    password: "asdf1234"
-},]
+const userData = []
 
 const seedUsers = () => User.bulkCreate(userData);
 
